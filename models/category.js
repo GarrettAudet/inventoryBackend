52ee0488d552385051202905f327a9bad51da789
GarrettAudet <70986283+GarrettAudet@users.noreply.github.com>
2024-06-07 13:32:02 -0400

    Defined models and virtuals for item and category

--- models/category.js
+++ models/category.js
@@ -1,5 +1,4 @@
 const mongoose = require("mongoose");
-const { DateTime } = require('luxon');
 
 const Schema = mongoose.Schema;
 
@@ -7,6 +6,16 @@ const CategorySchema = new Schema({
     name: { type: String, required: true },
     description: { type: String },
     url: { type: String }
+});
+
+// Virtual for Formatted Description
+CategorySchema.virtual("formattedDescription").get(function() {
+    return `${this.name}: ${this.description}`;
+});
+
+// Virtual for category's URL
+CategorySchema.virtual("standardUrl").get(function() {
+    return `/categories/${this._id}`;
 });
 
 // Export model
--- models/item.js
+++ models/item.js
@@ -1,5 +1,4 @@
 const mongoose = require("mongoose");
-const { DateTime } = require('luxon');
 
 const Schema = mongoose.Schema;
 
@@ -10,6 +9,26 @@ const ItemSchema = new Schema({
     price: { type: Number, required: true },
     number_in_stock: { type: Number, required: true },
     url: { type: String }
+});
+
+// Virtual for Full Description
+ItemSchema.virtual("fullDescription").get(function() {
+    return `${this.name} - ${this.description}, Priced at: $${this.price}`;
+});
+
+// Virtual for Availability 
+ItemSchema.virtual("isAvailable").get(function() {
+    return this.number_in_stock > 0 ? "In Stock" : "Out of Stock";
+});
+
+// Virtual for Formatted Price
+ItemSchema.virtual("formattedPrice").get(function() {
+    return `$${this.price.toFixed(2)}`;
+});
+
+// Virtual for a URL Path
+ItemSchema.virtual("standardUrl").get(function() {
+    return this.url ? this.url : `/items/${this._id}`;
 });
 
 // Export model
